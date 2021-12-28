00bd9d2704ccdf921ec9edb3c113a13e9123abfc
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-12-28 13:13:39 +0000

    parcialmente realizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,61 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Container, Form, Row, Table } from "reactstrap";
+import {
+	Button,
+	Container,
+	Form,
+	Modal,
+	ModalBody,
+	ModalFooter,
+	Row,
+	Table,
+} from "reactstrap";
 
 const dataTarea = [
+	/* 
 	{ id: 1, tarea: "Lavar mascota", fecha: "12 / 12 / 2021" },
 	{ id: 2, tarea: "Sacar a pasear mascota", fecha: "13 / 12 / 2021" },
 	{ id: 3, tarea: "Dar de comer a mascota", fecha: "14 / 12 / 2021" },
+ */
 ];
 
 //create your first component
 const Home = () => {
 	const [data, setData] = useState(dataTarea);
-	console.log(data);
+	const [modalEliminar, setModalEliminar] = useState(false);
+	const [tareaSeleccionada, setTareaSeleccionada] = useState({
+		id: "",
+		tarea: "",
+		fecha: "",
+	});
+	const seleccionarTarea = (elemento, caso) => {
+		setTareaSeleccionada(elemento);
+		caso === "Eliminar" && setModalEliminar(true);
+	};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setTareaSeleccionada((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+		console.log(tareaSeleccionada);
+	};
+	const eliminar = () => {
+		setData(data.filter((tarea) => tarea.id !== tareaSeleccionada));
+		setModalEliminar(false);
+	};
+
+	const limpiarSetTareaSeleccionada = () => {
+		setTareaSeleccionada(null);
+	};
+
+	const insertar = () => {
+		const valorIsertar = tareaSeleccionada;
+		valorIsertar.id = data[data.length - 1].id + 1;
+		const dataNueva = data;
+		dataNueva.push(valorIsertar);
+		setData(dataNueva);
+	};
 	return (
 		<>
 			<Container>
@@ -20,19 +64,38 @@ const Home = () => {
 				</Row>
 				<Row>
 					<Form>
-						{/* <Form.Group>
-							<Form.Label>ID</Form.Label>
-							<Form.Control name="id" disabled />
-						</Form.Group> */}
+						<div className="mb-3">
+							<label className="form-label">ID</label>
+							<input
+								className="form-control"
+								id=""
+								name=""
+								value={data.length + 1}
+								onChange={handleChange}
+								disabled
+							/>
+						</div>
 						<div className="mb-3">
 							<label className="form-label">Tarea</label>
-							<input className="form-control" id="" />
+							<input
+								className="form-control"
+								id=""
+								name="tarea"
+								onChange={handleChange}
+							/>
 						</div>
 						<div className="mb-3">
 							<label className="form-label">Fecha</label>
-							<input className="form-control" id="" />
+							<input
+								className="form-control"
+								name="fecha"
+								id=""
+								onChange={handleChange}
+							/>
 						</div>
-						<Button className="btn btn-success">
+						<Button
+							className="btn btn-success"
+							onClick={() => insertar()}>
 							Insertar Tarea
 						</Button>
 					</Form>
@@ -40,7 +103,7 @@ const Home = () => {
 			</Container>
 			<Container>
 				<Row>
-					<Table>
+					<Table className="">
 						<thead>
 							<tr>
 								<th>ID</th>
@@ -50,28 +113,53 @@ const Home = () => {
 							</tr>
 						</thead>
 						<tbody>
-							{data &&
-								data.map((elemento) => {
-									return (
-										<tr key={elemento.id}>
-											<td>{elemento.id}</td>
-											<td>{elemento.tarea}</td>
-											<td>{elemento.fecha}</td>
-											<td>
-												<Button className="btn btn-primary">
-													Editar
-												</Button>
-												<Button className="btn btn-danger">
-													Eliminar
-												</Button>
-											</td>
-										</tr>
-									);
-								})}
+							{data.map((elemento) => {
+								return (
+									<tr key={elemento.id}>
+										<td>{elemento.id}</td>
+										<td>{elemento.tarea}</td>
+										<td>{elemento.fecha}</td>
+										<td>
+											<Button className="btn btn-primary">
+												Editar
+											</Button>
+											{"  "}
+											<Button
+												className="btn btn-danger"
+												onClick={() =>
+													seleccionarTarea(
+														elemento,
+														"Eliminar"
+													)
+												}>
+												Eliminar
+											</Button>
+										</td>
+									</tr>
+								);
+							})}
 						</tbody>
 					</Table>
 				</Row>
 			</Container>
+			<Modal isOpen={modalEliminar}>
+				<ModalBody>
+					Estás Seguro que deseas eliminar Tarea:{" "}
+					{tareaSeleccionada && tareaSeleccionada.tarea}
+				</ModalBody>
+				<ModalFooter>
+					<button
+						className="btn btn-danger"
+						onClick={() => eliminar()}>
+						Sí
+					</button>
+					<button
+						className="btn btn-secondary"
+						onClick={() => setModalEliminar(false)}>
+						No
+					</button>
+				</ModalFooter>
+			</Modal>
 		</>
 	);
 };
