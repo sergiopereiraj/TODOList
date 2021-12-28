df135abee2eea52ebb4da6b775de182d2364c291
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-12-28 01:32:08 +0000

    guardado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,13 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Container, Form, Row, Table } from "reactstrap";
 
 const dataTarea = [
-	{ id: 1, tarea: "Lavar mascota", fecha: 12 / 12 / 2021 },
-	{ id: 1, tarea: "Sacar a pasear mascota", fecha: 13 / 12 / 2021 },
-	{ id: 1, tarea: "Dar de comer a mascota", fecha: 14 / 12 / 2021 },
+	{ id: 1, tarea: "Lavar mascota", fecha: "12 / 12 / 2021" },
+	{ id: 2, tarea: "Sacar a pasear mascota", fecha: "13 / 12 / 2021" },
+	{ id: 3, tarea: "Dar de comer a mascota", fecha: "14 / 12 / 2021" },
 ];
-/* const [data, setData] = useState(dataTarea); */
+
 //create your first component
 const Home = () => {
+	const [data, setData] = useState(dataTarea);
+	console.log(data);
 	return (
 		<>
 			<Container>
@@ -18,10 +20,10 @@ const Home = () => {
 				</Row>
 				<Row>
 					<Form>
-						<div className="mb-3">
-							<label className="form-label">ID</label>
-							<input className="form-control" id="" disabled />
-						</div>
+						{/* <Form.Group>
+							<Form.Label>ID</Form.Label>
+							<Form.Control name="id" disabled />
+						</Form.Group> */}
 						<div className="mb-3">
 							<label className="form-label">Tarea</label>
 							<input className="form-control" id="" />
@@ -48,21 +50,24 @@ const Home = () => {
 							</tr>
 						</thead>
 						<tbody>
-							{/* {data.map((elemento) => {
-								<tr key={elemento.id}>
-									<td>{elemento.id}</td>
-									<td>{elemento.tarea}</td>
-									<td>{elemento.fecha}</td>
-									<td>
-										<Button className="btn btn-primary">
-											Editar
-										</Button>
-										<Button className="btn btn-danger">
-											Eliminar
-										</Button>
-									</td>
-								</tr>;
-							})} */}
+							{data &&
+								data.map((elemento) => {
+									return (
+										<tr key={elemento.id}>
+											<td>{elemento.id}</td>
+											<td>{elemento.tarea}</td>
+											<td>{elemento.fecha}</td>
+											<td>
+												<Button className="btn btn-primary">
+													Editar
+												</Button>
+												<Button className="btn btn-danger">
+													Eliminar
+												</Button>
+											</td>
+										</tr>
+									);
+								})}
 						</tbody>
 					</Table>
 				</Row>
